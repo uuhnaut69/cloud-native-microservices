b8411d9e29de9d092cff267a04f75c059c123eb8
uuhnaut69 <uuhnaut96@gmail.com>
2025-06-14 11:12:15 +0700

    fix: cors catalog service

---
+++ src/catalog/src/main/java/com/uuhnaut69/catalog/infrastructure/configuration/WebConfig.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 uuhnaut69
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.uuhnaut69.catalog.infrastructure.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@EnableWebFlux
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+        .allowedHeaders("*")
+        .exposedHeaders("*")
+        .maxAge(3600L);
+  }
+}
